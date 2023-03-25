ebc62eee5da2d0cdf0be2743cfcd7c876b78f994
voomp <bat7gamer7@gmail.com>
2023-03-25 10:06:42 -0400

    integrating tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className="login-main">
-        <div className="bg-img">
+      <div className="login-main flex w-full h-full">
+        <div className="bg-img w-2/5 h-full min-h-full bg-[url('./graphic.png')] bg-center bg-no-repeat bg-cover relative">
         </div>
-        <div className="login-sec">
-          <div className="login-container">
+        <div className="login-sec w-3/5 h-full bg-[#1D203E] flex justify-center">
+          <div className="login-container self-center w-3/5 h-4/5 bg-[#2C2F48]">
             
           </div>
         </div>
