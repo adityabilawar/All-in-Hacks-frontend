6420894ac1535f2df77724231a78ce72e6dbb803
voomp <bat7gamer7@gmail.com>
2023-03-25 18:03:56 -0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,35 @@
-import { useState } from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import LoginPage from './pages/Login';
 
 function App() {
 
-  const [name, setName] = useState('');
-  const [pass, setPass] = useState('');
+  const [auth, setAuth] = useState<null | boolean>(null);
 
-  return (
-    <>
-      <div className="login-main flex w-full h-full">
-        <div className="bg-img w-2/5 h-full min-h-full bg-[url('./graphic.png')] bg-center bg-no-repeat bg-cover relative">
-        </div>
-        <div className="login-sec w-3/5 h-full bg-[#1D203E] flex justify-center">
-          <div className="login-container self-center w-3/5 h-4/5 bg-[#2C2F48] flex flex-col items-center justify-around">
-            <div className="login-title font-semibold text-slate-200 text-4xl">
-              Log In
-            </div>
-            <div className="login-form flex flex-col gap-5">
-              <input 
-                type="text"
-                placeholder="Username"
-                onChange={e => setName(e.target.value)}
-                value={name}
-              />
-              <input 
-                type="password"
-                placeholder="Password"
-                onChange={e => setPass(e.target.value)}
-                value={pass}
-              />
-            </div>
-            <div className="login-btn p-5 text-2xl text-indigo-200 rounded-3xl font-semibold cursor-pointer select-none">
-              SUBMIT
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  useEffect(() => {
+    setAuth((localStorage.getItem('auth') !== null))
+  }, [])
+
+  if(auth === false) {
+    return (
+      <LoginPage />
+    )
+  }
+
+  else if(auth) {
+    return (
+      <>
+        Logged In
+      </>
+    )
+  }
+
+  else {
+    return (
+      <>
+      </>
+    )
+  }
+  
 }
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import '../Login.css';
+
+const LoginPage = () => {
+
+  const [name, setName] = useState('');
+  const [pass, setPass] = useState('');
+
+  const submitLogin = () => {
+    console.log(name, pass);
+	localStorage.setItem('auth', 'true');
+	window.location.reload();
+  }
+
+  return (
+    <>
+      <div className="login-main flex w-full h-full">
+        <div className="bg-img w-2/5 h-full min-h-full bg-[url('./graphic.png')] bg-center bg-no-repeat bg-cover relative">
+        </div>
+        <div className="login-sec w-3/5 h-full flex justify-center">
+          <div className="login-container self-center w-3/5 h-4/5 bg-[#2C2F48] flex flex-col items-center justify-evenly">
+            <div className="login-title font-semibold text-slate-200 text-6xl">
+              Log In
+            </div>
+            <div className="login-form flex flex-col gap-5">
+              <input 
+                type="text"
+                placeholder="Username"
+                onChange={e => setName(e.target.value)}
+                value={name}
+                className="loginInput p-2 pr-24 text-xl text-[#f1f5f9] border-b-2 border-solid border-[#e0f2fe] focus:border-[#818cf8]"
+              />
+              <input 
+                type="password"
+                placeholder="Password"
+                onChange={e => setPass(e.target.value)}
+                value={pass}
+                className="loginInput p-2 pr-24 text-xl text-[#f1f5f9] border-b-2 border-solid border-[#e0f2fe] focus:border-[#818cf8]"
+              />
+            </div>
+            <div className="login-btn p-5 text-2xl text-indigo-200 rounded-3xl font-semibold cursor-pointer select-none" onClick={submitLogin}>
+              SUBMIT
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LoginPage;
